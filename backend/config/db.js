f1b3b9eafee271028017760779f881df90187576
Zaklaria <zsumonn@gmail.com>
2021-10-11 12:29:44 +0600

    Product created testin fine

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = () => {
+	mongoose
+		.connect(process.env.DB_URI, { useNewUrlParser: true })
+		.then((data) => {
+			console.log(`Mongodb connected with server: ${data.connection.host}`);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 // config
 dotenv.config({ path: 'backend/config/config.env' });
+
+// database connection
+connectDB();
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server is working on http://localhost:${process.env.PORT}`);
