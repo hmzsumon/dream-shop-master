a69a946a2c0a76d70173577b8ded039074adbfa0
Zaklaria <zsumonn@gmail.com>
2021-10-13 11:06:27 +0600

    reset password is workink fine

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ const catchAsyncErrors = require('../middleware/catchAsyncErrors');
 const User = require('../models/userModel');
 const sendToken = require('../utils/jwtToken');
 const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');
 
 // Register a user
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -73,7 +74,7 @@ exports.forgetPassword = catchAsyncErrors(async (req, res, next) => {
 	// rest password url ex: http://localhost:5000/api/v1/password/reset
 	const resetPasswordUrl = `${req.protocol}://${req.get(
 		'host'
-	)}api/v1/password/reset/${restToken}`;
+	)}/api/v1/password/reset/${restToken}`;
 
 	const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
 
@@ -96,4 +97,39 @@ exports.forgetPassword = catchAsyncErrors(async (req, res, next) => {
 
 		return next(new ErrorHandler(error.message, 500));
 	}
+});
+
+// Reset Password
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+	// creating token hash
+	const resetPasswordToken = crypto
+		.createHash('sha256')
+		.update(req.params.token)
+		.digest('hex');
+
+	const user = await User.findOne({
+		resetPasswordToken,
+		resetPasswordExpire: { $gt: Date.now() },
+	});
+
+	if (!user) {
+		return next(
+			new ErrorHandler(
+				'Reset Password Token is invalid or has been expired',
+				400
+			)
+		);
+	}
+
+	if (req.body.password !== req.body.confirmPassword) {
+		return next(new ErrorHandler('Password does not password', 400));
+	}
+
+	user.password = req.body.password;
+	user.resetPasswordToken = undefined;
+	user.resetPasswordExpire = undefined;
+
+	await user.save();
+
+	sendToken(user, 200, res);
 });
